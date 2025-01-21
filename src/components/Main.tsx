53023d0bd3e2b14b56460ca60db6c88110b0f0b9
Mariusz <casmar90@gmail.com>
2025-01-21 17:06:55 +0100

    refactor layout spacing in Footer, Header, and Main components for improved responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 const Main = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex-1 bg-gray-100">
-      <div className="mx-auto grid h-full w-full max-w-[1280px] content-start p-8">
+      <div className="mx-auto grid h-full w-full max-w-[1280px] content-start sm:p-8">
         {children}
       </div>
     </main>
